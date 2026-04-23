78fd186bd2b31269bd800b01a2b1914372b7c67a
fabianelyorch <168842981+fabianelyorch@users.noreply.github.com>
2026-04-23 16:41:00 -0600

    Add files via upload

---
+++ space_invaders.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    int puntos;
+    bool estaActivo;
+    char simbolo;
+} Enemigo;
+
+typedef struct Proyectil {
+    int posX, posY;
+    struct Proyectil* siguiente;
+} Proyectil;
+
+typedef struct {
+    Enemigo** horda;
+    int filas;
+    int columnas;
+    int navePos;
+    Proyectil* listaProyectiles; 
+} Juego;
+
+void inicializarNivel(Juego* partida, int f, int c);
+void dispararJugador(Proyectil** cabeza, int xActual, int yActual);
+void liberarMemoria(Juego* partida);
+
+int main() {
+    Juego miPartida;
+    int filasConfig = 5;
+    int columnasConfig = 10;
+
+    inicializarNivel(&miPartida, filasConfig, columnasConfig);
+
+    dispararJugador(&miPartida.listaProyectiles, miPartida.navePos, 20);
+
+    liberarMemoria(&miPartida);
+
+    return 0;
+}
+
+void inicializarNivel(Juego* partida, int f, int c) {
+    partida->filas = f;
+    partida->columnas = c;
+    partida->navePos = c / 2;
+    partida->listaProyectiles = NULL;
+
+    partida->horda = (Enemigo**)malloc(f * sizeof(Enemigo*));
+    
+    for (int i = 0; i < f; i++) {
+        partida->horda[i] = (Enemigo*)malloc(c * sizeof(Enemigo));
+        
+        for (int j = 0; j < c; j++) {
+            partida->horda[i][j].estaActivo = true;
+            partida->horda[i][j].simbolo = 'W';
+            partida->horda[i][j].puntos = 10;
+        }
+    }
+    printf("[OK] Matriz de %dx%d creada dinamicamente.\n", f, c);
+}
+
+void dispararJugador(Proyectil** cabeza, int xActual, int yActual) {
+    Proyectil* nuevoProyectil = (Proyectil*)malloc(sizeof(Proyectil));
+    
+    if (nuevoProyectil != NULL) {
+        nuevoProyectil->posX = xActual;
+        nuevoProyectil->posY = yActual;
+        
+        nuevoProyectil->siguiente = *cabeza;
+        *cabeza = nuevoProyectil;
+        
+        printf("[EVENTO] Disparo generado en X:%d, Y:%d\n", xActual, yActual);
+    }
+}
+
+void liberarMemoria(Juego* partida) {
+    for (int i = 0; i < partida->filas; i++) {
+        free(partida->horda[i]);
+    }
+    free(partida->horda);
+
+    Proyectil* actual = partida->listaProyectiles;
+    while (actual != NULL) {
+        Proyectil* temporal = actual;
+        actual = actual->siguiente;
+        free(temporal);
+    }
+    printf("[CLEAN] Memoria liberada correctamente.\n");
+}
